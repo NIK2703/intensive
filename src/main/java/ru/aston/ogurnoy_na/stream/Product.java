c12427dc26d5ba5e24e80a07dbdc10bb2df202ea
Nikita <nikita.ogurnoy@mail.ru>
2025-02-11 19:05:45 +0600

    stream tasks

---
+++ src/main/java/ru/aston/ogurnoy_na/stream/Product.java
@@ -0,0 +1,25 @@
+package ru.aston.ogurnoy_na.stream;
+
+public class Product {
+    String name;
+    String category;
+    double price;
+
+    public Product(String name, String category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
